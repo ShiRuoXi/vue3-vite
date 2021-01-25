6a0905ac89f03f6c0456c635b5e4d2b446225836
石小白 <34360952+ShiRuoXi@users.noreply.github.com>
2021-01-25 18:12:24 +0800

    添加axios-未完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from "./router";
+import Axios from "axios";
 
 
-createApp(App).use(Router).mount('#app')
+
+createApp(App).use(Router).use(Axios).mount('#app')
